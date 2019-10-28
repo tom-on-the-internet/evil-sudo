205d8d36d00e6ff552ffd37d6e12979153c0f6ba
tom <thomaspatricksteven@gmail.com>
2019-10-27 21:49:22 -0400

    Rewrite using directory creation
    Instead of trying to find a local directory on the PATH,
    we now make one instead

--- evil-sudo.sh
+++ evil-sudo.sh
@@ -1,48 +1,49 @@
 #!/usr/bin/env bash
-
-USER_DIRECTORY_IN_PATH=""
+SHELL_CONFIG_FILE=""
 FAKE_ERROR_MESSAGE="10d20d7815 EXCEPTION unknown \(0313\)"
 
-# Get the first local directory in the path
-set_user_directory_in_path() {
-    IFS=:
-    for section in $PATH; do
-        if [[ $section == *$USER* ]]; then
-            USER_DIRECTORY_IN_PATH=$section
-            break
-        fi
-    done
-}
-
-exit_if_no_user_directory_in_path() {
-    if [ -z "$USER_DIRECTORY_IN_PATH" ]; then
-        echo "Error: No user directory found in \$PATH."
-        exit 1
-    fi
-}
-
 spawn_evil_sudo() {
-    exec 3<>$USER_DIRECTORY_IN_PATH/sudo
+    evil_sudo_directory=~/.evil-sudo
 
-    echo "# Evil Sudo" >&3
+    # create directory
+    mkdir $evil_sudo_directory
+
+    # create sudo file
+    exec 3<>$evil_sudo_directory/sudo
+
+    echo "# I am Evil Sudo" >&3
     echo "sleep 1" >&3
     echo "echo $FAKE_ERROR_MESSAGE" >&3
 
     exec 3>&-
 
-    chmod +x $USER_DIRECTORY_IN_PATH/sudo
+    chmod +x $evil_sudo_directory/sudo
+
+    # add evil sudo directory to path
+    echo "PATH=$evil_sudo_directory:\$PATH #evil sudo" >>$SHELL_CONFIG_FILE
 
     echo 😈
 }
 
 clean_up() {
-    if test -f $USER_DIRECTORY_IN_PATH/sudo && grep -q "Evil Sudo" $USER_DIRECTORY_IN_PATH/sudo; then
-        rm $USER_DIRECTORY_IN_PATH/sudo
+    # remove evil sudo directory
+    rm -rf ~/.evil-sudo
+
+    # remove directory from path
+    sed -i '/evil sudo/d' $SHELL_CONFIG_FILE
+}
+
+set_shell_config_file() {
+    if [[ $SHELL == *"zsh" ]]; then
+        SHELL_CONFIG_FILE=~/.zshrc
+    elif [ $SHELL == *"bash" ]; then
+        SHELL_CONFIG_FILE=~/.bashrc
+    else
+        SHELL_CONFIG_FILE=~/.profile
     fi
 }
 
-set_user_directory_in_path
-exit_if_no_user_directory_in_path
+set_shell_config_file
 
 # Clean up if asked
 if [ "$1" == "--clean" ] || [ "$1" == "-c" ]; then
